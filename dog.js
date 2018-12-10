209d2f088257103dc38546be167e35c66133ee30
ashish <expecto30@gmail.com>
2018-12-09 22:30:56 -0600

    final commit

--- dog.js
+++
@@ -1,5 +0,0 @@
-check = str => {
-  if (str == "4") return true;
-};
-
-console.log(check(4));
--- server.js
+++ server.js
@@ -1,16 +1,14 @@
-var request = require("request");
-var express = require("express");
-var bodyparser = require("body-parser");
-const { dialogflow } = require("actions-on-google");
-var app = express();
+const express = require("express");
 const mysql = require("mysql2");
-//
+const bodyParser = require("body-parser");
+const app = express();
+const port = process.env.PORT || 5000;
 
-app.use(bodyparser.json());
-app.use(bodyparser.urlencoded({ extended: true }));
-app.set("port", process.env.PORT || 5000);
+let fname = "Ananda";
+let lname = "Poudel";
 
-var con = mysql.createConnection({
+app.use(bodyParser.json());
+var connection = mysql.createConnection({
   host: "35.193.222.192",
   port: 3306,
   user: "root",
@@ -18,36 +16,141 @@ var con = mysql.createConnection({
   database: "taskdb"
 });
 
-con.connect(function(err) {
-  if (err) throw err;
-  console.log("Connected!");
+app.get("/", (req, res) =>
+  res.json({
+    project: "task-api",
+    message: "Welcome! to our new LMS",
+    time: Date.now()
+  })
+);
+
+app.get("/post-water-data", async (req, res) => {
+  const dayCount = req.query.dayCount;
+  const waterLevel = req.query.waterLevel;
+  try {
+    const waterData = await connection
+      .promise()
+      .query(
+        `INSERT INTO water (dayCount,moistureLevel) Values (${dayCount},${waterLevel})`
+      );
+    res.send({ result: "Sucessfully added a new record!!" });
+  } catch (err) {
+    res.send({ result: "Could not insert data!!" });
+  }
 });
 
-app.post("/webhook", (req, res) => {
+app.get("/get-water-data", async (req, res) => {
+  const waterData = await connection.promise().query(`select * from water`);
+  res.json(waterData[0]);
+});
+// -------------------------------------------------------------------------------------------
+
+app.post("/webhook", async (req, res) => {
+  console.log("000000000000000000000000000");
+  console.log("000000000000000000000000000");
+  console.log("000000000000000000000000000");
+  console.log("000000000000000000000000000");
+  console.log("000000000000000000000000000");
+  console.log(req.body);
   console.log("Send the code my way bro!");
   if (!req.body) return res.sendStatus(400);
 
   res.setHeader("Content-Type", "application/json");
-  console.log(req.body.result);
 
-  return res.json({
-    speech: "luces apagadas",
-    displayText: "luces apagadas",
-    source: "webhook-echo-sample"
-  });
+  console.log(req.body.result.metadata.intentName);
+
+  var intent = req.body.result.metadata.intentName;
+  if (intent == "getGradeForClass") {
+    console.log("inside", intent);
+    //DONE
+    const course = req.body.result.parameters.class;
+    const data = await connection.promise()
+      .query(`select * from StudentGrade where CourseNum = (select CourseNum from Course where CourseTitle = '${course}') and StudentNum = (select StudentNum from Student where FirstName = '${fname}' and LastName = '${lname}')
+    `);
+    return res.json({
+      speech: `The grade that you have in ${
+        data[0][0].CourseNum
+      }  ${course} is ${data[0][0].Grade}`,
+      displayText: `The grade that you have in ${
+        data[0][0].CourseNum
+      }  ${course} is ${data[0][0].Grade}`,
+      source: "webhook-echo-sample"
+    });
+  } else if (intent == "getTheGrade") {
+    console.log("inside", intent);
+    //NOT DONE
+
+    const course = req.body.result.parameters.class;
+    const assignment = req.body.result.parameters.assignment;
+    const number = req.body.result.parameters.number;
+    console.log(course, " ", number);
+    const data = await connection.promise()
+      .query(`select * from Assignment where CourseNum = (select CourseNum from Course where CourseTitle = '${course}') and AssignmentNum = '${number}'
+  `);
+
+    console.log(data[0][0]);
+
+    return res.json({
+      speech: `You have gotten ${data[0][0].ObtainedPoints} out of ${
+        data[0][0].TotalPoints
+      } in  ${data[0][0].CourseNum} ${course} for ${assignment} ${number}`,
+      displayText: `You have gotten ${data[0][0].ObtainedPoints} out of ${
+        data[0][0].TotalPoints
+      } in  ${data[0][0].CourseNum} ${course} for ${assignment} ${number}`,
+      source: "webhook-echo-sample"
+    });
+  } else if (intent == "getGpa") {
+    console.log("inside", intent);
+    ///DONEs
+
+    const data = await connection.promise()
+      .query(`select * from Student where FirstName = '${fname}' and LastName = '${lname}'
+    `);
+
+    return res.json({
+      speech: `${fname} ${lname} has a Grade point average of ${
+        data[0][0].GPA
+      }`,
+      displayText: `${fname} ${lname} has a Grade point average of ${
+        data[0][0].ClassStanding
+      }`,
+      source: "webhook-echo-sample"
+    });
+  } else if (intent == "getTeacher") {
+    console.log("inside", intent);
+    //////DONE
+    const className = req.body.result.parameters.class;
+    const data = await connection.promise()
+      .query(`select * from Faculty where FacultyNum = (select FacultyNum from Section where CourseNum = (select CourseNum from Course where CourseTitle= '${className}'))
+    `);
+
+    return res.json({
+      speech: `The teacher for the class ${className} is  ${
+        data[0][0].LastName
+      } ${data[0][0].FirstName}`,
+      displayText: `The teacher for the class ${className} is  ${
+        data[0][0].LastName
+      } ${data[0][0].FirstName}`,
+      source: "webhook-echo-sample"
+    });
+  } else if (intent == "changeStudent") {
+    fname = req.body.result.parameters.firstname;
+    lname = req.body.result.parameters.lastname;
+    console.log("inside", intent);
+    //NOT DONE
+    return res.json({
+      speech: `Student has been changed to ${fname} ${lname}`,
+      displayText: `Student has been changed to ${fname} ${lname}`,
+      source: "webhook-echo-sample"
+    });
+  } else {
+    console.log("inside", intent);
+    return res.json({
+      speech: "Can you say that again bro?",
+      displayText: "mmmmm",
+      source: "webhook-echo-sample"
+    });
+  }
 });
 
-app.get("/student-grade", async (req, res) => {
-  const className = req.query.className;
-  const studentNumber = await con
-    .promise()
-    .query(
-      `select StudentNum from Student where FirstName = "Ananda" and LastName = "Poudel"`
-    );
-  console.log(studentNumber[0]);
-  res.json(studentNumber[0][0]);
-});
-
-app.listen(app.get("port"), () => {
-  console.log("Express server started on port", app.get("port"));
-});
+app.listen(port, () => console.log(`task listening on port ${port}!`));
